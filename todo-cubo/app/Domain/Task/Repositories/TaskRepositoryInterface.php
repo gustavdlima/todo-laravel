d350be2be84623012dd4b3fcf4699f75aeca3e4e
gustavdlima <gustavdlima@gmail.com>
2025-03-17 10:17:32 -0300

    Created TaskRepositoryInterface and EloquentTaskRepository

---
+++ todo-cubo/app/Domain/Task/Repositories/TaskRepositoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Domain\TaskManagement\Repositories;
+
+use App\Domain\TaskManagement\Entities\Task;
+use App\Domain\TaskManagement\ValueObjects\TaskStatus;
+use App\Domain\TaskManagement\ValueObjects\SortDirection;
+
+use DateTimeInterface;
+
+interface TaskRepositoryInterface
+{
+    public function findById(int $id): ?Task;
+    public function findAll(int $userId): array;
+    public function findByStatus(int $userId, TaskStatus $status): array;
+    public function findByDateRange(int $userId, DateTimeInterface $start, DateTimeInterface $end): array;
+	public function findByCreationDate(int $userId, DateTimeInterface $date): array;
+	public function findAllOrderedByCreationDate(int $userId, SortDirection $directio = null): array;
+	public function findAllOrderedByStatus(int $userId, SortDirection $direction = null): array;
+    public function save(Task $task): Task;
+    public function delete(int $id): bool;
+    public function addComment(int $taskId, string $content, int $userId): void;
+    public function getComments(int $taskId): array;
+}
