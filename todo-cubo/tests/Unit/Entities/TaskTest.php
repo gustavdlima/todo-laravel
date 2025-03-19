a1efcadc73c10467e9d3dc0f66f77c0c671e64e4
gustavdlima <gustavdlima@gmail.com>
2025-03-19 00:33:44 -0300

    Add unit test for adding a comment in TaskTest

--- todo-cubo/tests/Unit/Entities/TaskTest.php
+++ todo-cubo/tests/Unit/Entities/TaskTest.php
@@ -12,11 +12,12 @@ use InvalidArgumentException;
 class TaskTest extends TestCase
 {
     /**
-     * Testa a criação de uma tarefa com valores válidos
+     * Tests the creation of a task with valid values
      */
     public function test_can_create_task_with_valid_values()
     {
         $task = new Task(
+            id: 1,
             title: 'Implement API',
             description: 'Develop endpoints',
             status: TaskStatus::PENDING(),
@@ -24,11 +25,34 @@ class TaskTest extends TestCase
             dueDate: new DateTime('2023-12-31'),
         );
 
+        $this->assertEquals(1, $task->getId());
         $this->assertEquals('Implement API', $task->getTitle());
         $this->assertEquals('Develop endpoints', $task->getDescription());
         $this->assertEquals(TaskStatus::PENDING(), $task->getStatus());
         $this->assertEquals(1, $task->getUserId());
         $this->assertInstanceOf(DateTimeInterface::class, $task->getDueDate());
+    }
+
+     /**
+     * Test that adding a comment works correctly
+     */
+    public function test_can_add_comment()
+    {
+        $task = new Task(
+            id: 1,
+            title: 'Implement API',
+            description: 'Develop endpoints',
+            status: TaskStatus::PENDING(),
+            userId: 1,
+            dueDate: new DateTime('2023-12-31'),
+        );
+
+        $this->assertEquals(1, $task->getId());
+        $task->addComment('Test login endpoint', 1, $task->getId());
+        $comments = $task->getComments();
+        $this->assertCount(1, $comments);
+        $this->assertEquals('Test login endpoint', $comments[0]->getContent());
+        $this->assertEquals(1, $comments[0]->getUserId());
     }
 
 }
